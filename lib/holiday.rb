af5c809e49d864a57e4a553817954d23ebbe3c1a
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-11 01:51:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
           :memorial_day => ["BBQ"]
         }}
         
-        
+        holiday_hash[:season]={}
+        holiday_hash[:season][]
   # remember to return the updated hash
 
 end

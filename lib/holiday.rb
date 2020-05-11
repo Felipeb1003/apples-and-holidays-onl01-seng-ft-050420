cd95090841f0da224d49efa416ac849dffcc70bc
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-11 01:27:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -15,7 +15,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
     :spring => {
       :memorial_day => ["BBQ"]
     }}
- holiday_supplies[:summer][:fourth_of_july][1]
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)

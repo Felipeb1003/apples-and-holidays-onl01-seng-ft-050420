1c9404c82b6dd8d5ba109d17d52ce034d9d4ce4f
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-11 01:53:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
         }}
         
         holiday_hash[:season]={}
-        holiday_hash[:season][:holiday_name] = [supply_array]
+        holiday_hash[:season][:holiday_name]= [supply_array]
         holiday_hash
   # remember to return the updated hash
 
